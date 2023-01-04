8df761e7e865732be9059ac4a7782a687533b715
KairosJK <jonathan.kocevar@gmail.com>
2023-01-03 19:39:22 -0500

    Fixed ssh key

--- nixos/laptop/configuration.nix
+++ nixos/laptop/configuration.nix
@@ -10,7 +10,7 @@
 
   users.users.${user} = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAU8smpJwYEwdCreRsnmYPDO0vWtWbM6EXIVYJwaqwu/ ultragauche@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAjLANnZwmrSwpptvyAfulNYZdUfbmSYEqhgWBpMvMGr ultragauche@desktop"
     ];
   };
 }
